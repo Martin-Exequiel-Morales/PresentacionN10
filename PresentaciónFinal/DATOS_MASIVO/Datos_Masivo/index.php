c44004ff39a89bff191eaeedcfb193ef01207b84
AlanMaggiolo <healer_alan@hotmail.com>
2021-08-15 01:37:28 -0300

    cambios alan importado de csv

---
+++ PresentaciónFinal/DATOS_MASIVO/Datos_Masivo/index.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+    <title></title>
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaina" rel="stylesheet">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+    <center><strong><label class="titulo">IMPORTAR carreras .CSV</label></strong></center>
+    <p>
+    <form action="subir_archivo.php" method="POST" enctype="multipart/form-data">
+        <center>
+            <table>
+                <tr>
+                    <td class="letra" width="250"><strong>Subir carreras:</strong></td>
+                    <td><input type="file" name="foto" id="foto"></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" name="enviar" value="SUBIR" class="boton"></td>
+                </tr>
+            </table>
+        </center>
+    </form>
+    <form action="subir_materias.php" method="POST" enctype="multipart/form-data">
+        <center>
+            <table>
+                <tr>
+                    <td class="letra" width="250"><strong>Subir materias:</strong></td>
+                    <td><input type="file" name="foto" id="foto"></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" name="enviar" value="SUBIR" class="boton"></td>
+                </tr>
+            </table>
+        </center>
+    </form>
+
+</body>
+
+</html>
